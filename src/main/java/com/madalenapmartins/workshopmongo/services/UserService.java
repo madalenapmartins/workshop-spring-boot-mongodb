a28c741b97b63e018073c214ffb8968c87e2490c
Madalena Martins <madalena_parreira_martins@hotmail.com>
2024-09-05 12:17:51 +0100

    User insertion with POST

--- src/main/java/com/madalenapmartins/workshopmongo/services/UserService.java
+++ src/main/java/com/madalenapmartins/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.madalenapmartins.workshopmongo.domain.User;
+import com.madalenapmartins.workshopmongo.dto.UserDTO;
 import com.madalenapmartins.workshopmongo.repositories.UserRepository;
 import com.madalenapmartins.workshopmongo.services.exceptions.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
