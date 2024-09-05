656996322a6a7f22e92003c65d4d84f473da37d5
Madalena Martins <madalena_parreira_martins@hotmail.com>
2024-09-05 15:08:04 +0100

    Getting a post by id

---
+++ src/main/java/com/madalenapmartins/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.madalenapmartins.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.madalenapmartins.workshopmongo.domain.Post;
+import com.madalenapmartins.workshopmongo.repositories.PostRepository;
+import com.madalenapmartins.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> post = repo.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
